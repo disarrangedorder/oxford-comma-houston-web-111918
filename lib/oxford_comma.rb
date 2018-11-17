fe363b16f960358c49d308b4a4485807a13bca54
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 19:16:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,12 +5,10 @@ def oxford_comma(array)
   elsif array.length == 2
     string << array.join(" and ")
   else array.length >=3
-    
+
     last_entry = array.pop #
     last_entry.toString()
     last_entry  = "and #{lastentry}."
-    join_last_entry = array.push(last_entry)
-    new_string = array.join(", ")
   end
   return new_string
 
